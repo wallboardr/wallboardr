fe162f107ea8983e6b85f59d0e2ebedd8b68cd07
Colin Bate <1060912+colinbate@users.noreply.github.com>
2013-07-06 03:53:27 -0300

    Initial structure, sproute tests seem positive.

---
+++ index.js
@@ -0,0 +1,27 @@
+var Sproute = require("./sproute/app");
+var http = require('http');
+
+var app = new Sproute(".");
+app.hooks.fetch = function (block, next) {
+  //pause parsing and decode request
+  var expr = block.expr.split(" ");
+  var url = expr[0];
+  console.log("FETCH", url);
+  //request the data then continue parsing
+  var html = '';
+  if (!url) {
+    next();
+    return;
+  } 
+  http.get(url, function (res) {
+    res.on('data', function (chunk) {
+      html += chunk;
+    });
+    res.on('end', function () {
+      next(html);
+    });
+  }).on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+};
+
