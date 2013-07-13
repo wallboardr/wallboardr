ff1ff06f1d26bf48e204febb278bc6dab9151d77
Colin Bate <1060912+colinbate@users.noreply.github.com>
2013-07-13 17:45:47 -0300

    Added first controller for main app body.

---
+++ assets/js/app/controllers/wallboardr.js
@@ -0,0 +1,12 @@
+define([], function () {
+    'use strict';
+
+    var wallboardrController = function ($scope) {
+        $scope.menu = [
+            {url: '/admin', label: 'Admin'}
+        ];
+    };
+    wallboardrController.$inject = ['$scope'];
+
+    return wallboardrController;
+});
