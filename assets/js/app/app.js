c5b71d4d51ae953b27b51d2b72de11480263b9c4
c.bate <c.bate@localhost>
2013-07-13 17:45:47 -0300

    Added first controller for main app body.

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,11 +1,22 @@
-define(['angular'], function (angular) {
+define(['angular', 'controller/wallboardr'], function (angular, wallboardrCtrl) {
     'use strict';
+
+    var appName = 'wallboardr',
+        log = function () {
+            var msg = Array.prototype.join.call(arguments, ' ');
+            if (window.console) {
+                window.console.log(msg);
+            }
+        };
 
     return {
         bootstrap: function () {
-            if (window.console) {
-                window.console.log('Tower, this is Ghost Rider requesting a flyby.')
-            }
+            var app;
+            log('Tower, this is Ghost Rider requesting a flyby.')
+            app = angular.module(appName, []);
+            app.controller('WallboardrController', wallboardrCtrl);
+            angular.bootstrap(document, [appName]);
+            log('Negative Ghost Rider, the pattern is full.');
         }
     };
 });
