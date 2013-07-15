425edc1a5ee7ccb0a37b5efb18b5c6ed8bdc2090
Colin Bate <1060912+colinbate@users.noreply.github.com>
2013-07-15 00:19:10 -0300

    You can now create a new board via a form.

--- assets/js/app/controllers/index.js
+++ assets/js/app/controllers/index.js
@@ -1,9 +1,40 @@
-define([], function () {
+define(['angular'], function (angular) {
   'use strict';
-  var indexController = function ($scope) {
+  var indexController = function ($scope, $http) {
+    var resetNewBoard = function (board) {
+      board.name = '';
+      board.slug = '';
+      board.desc = '';
+      board.duration = '';
+    };
+
     $scope.greeting = 'Colin';
+    $scope.boards = [];
+
+    $scope.loadBoards = function () {
+      var url = '/data/boards';
+      $http.get(url).success(function (data) {
+        $scope.boards = data;
+      });
+    };
+    $scope.loadBoards();
+
+    $scope.addBoard = function (board) {
+      var newBoard, bIndex;
+      if (board.$valid) {
+        newBoard = angular.copy(board);
+        $http.post('/data/boards', newBoard).success(function (data) {
+          if (data && angular.isArray(data)) {
+            for (bIndex = 0; bIndex < data.length; bIndex += 1) {
+              $scope.boards.push(data[bIndex]);
+            }
+          }
+        });
+        resetNewBoard(board);
+      }
+    };
   };
-  indexController.$inject = ['$scope'];
+  indexController.$inject = ['$scope', '$http'];
 
   return indexController;
 });
