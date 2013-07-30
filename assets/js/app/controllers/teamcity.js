2e0de909a12788f6435dccf2deb92ae5a19fc90e
c.bate <c.bate@localhost>
2013-07-30 17:55:54 -0300

    Slowing pulling in the teamcity api

--- assets/js/app/controllers/teamcity.js
+++ assets/js/app/controllers/teamcity.js
@@ -1,10 +1,16 @@
-define([], function () {
+define(['teamcity-api'], function (Teamcity) {
   'use strict';
   var teamcityController = function ($scope, dataLoader) {
+    var tcApi;
     $scope.reset = function () {
       $scope.projects = [];
       $scope.tcUrl = '';
-    }
+    };
+    $scope.connectToTeamcity = function () {
+        if ($scope.url) {
+            tcApi = new Teamcity($scope.url, dataLoader);
+        }
+    };
     $scope.reset();
 
   };
