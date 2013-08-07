bda6c002c9d0dc3fafb2d7409fc2b12b5c78c9eb
c.bate <c.bate@localhost>
2013-08-07 14:30:52 -0300

    Allow screens to be disabled

--- assets/js/boards/screens/factory.js
+++ assets/js/boards/screens/factory.js
@@ -3,7 +3,9 @@ define(['screen/local', 'screen/html', 'screen/teamcity'], function (local, html
   var screenTypes = { local: local, html: html, teamcity: teamcity },
       factory = function (boardProps) {
         return function (screen) {
-          return screenTypes[screen.type] ? screenTypes[screen.type](screen, boardProps) : undefined;
+          return !screen.disabled && screenTypes[screen.type] ?
+                    screenTypes[screen.type](screen, boardProps)
+                    : undefined;
         };
       };
   return factory;
