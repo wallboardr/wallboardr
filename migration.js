6e4677f4809f792e43ebd9e4b90183f317e54c39
Colin Bate <1060912+colinbate@users.noreply.github.com>
2013-11-28 10:43:54 -0400

    Add migration script for database.

---
+++ migration.js
@@ -0,0 +1,2 @@
+db.screens.find().forEach(function(doc){ var id=doc._id; if(id.str){doc._id=id.str; db.screens.insert(doc); db.screens.remove({_id:id})} })
+db.boards.find().forEach(function(doc){ var id=doc._id; if(id.str){doc._id=id.str; db.boards.remove({_id:id}); db.boards.insert(doc);} })
