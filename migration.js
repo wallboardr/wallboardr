644bc80f5cf22bd89a8988bf4fbcb6f2e31c2346
Colin Bate <1060912+colinbate@users.noreply.github.com>
2013-11-28 15:25:24 -0400

    Remove teamcity code to its own repo as a plugin

--- migration.js
+++ migration.js
@@ -1,2 +1,3 @@
-db.screens.find().forEach(function(doc){ var id=doc._id; if(id.str){doc._id=id.str; db.screens.insert(doc); db.screens.remove({_id:id})} })
-db.boards.find().forEach(function(doc){ var id=doc._id; if(id.str){doc._id=id.str; db.boards.remove({_id:id}); db.boards.insert(doc);} })
+db.copyDatabase('wallboardr', 'bkpwallboardr');
+db.screens.find().forEach(function(doc){ var id=doc._id; if(id.str){doc._id=id.str; db.screens.insert(doc); db.screens.remove({_id:id})} });
+db.boards.find().forEach(function(doc){ var id=doc._id; if(id.str){doc._id=id.str; db.boards.remove({_id:id}); db.boards.insert(doc);} });
