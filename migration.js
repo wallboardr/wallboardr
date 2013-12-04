8823f982d101f2b99e63c3ca1df1a2c8acd4ca09
Colin Bate <1060912+colinbate@users.noreply.github.com>
2013-12-04 16:11:07 -0400

    Get the proxy resource working.

--- migration.js
+++ migration.js
@@ -3,3 +3,5 @@ db.screens.find().forEach(function(doc){ var id=doc._id; if(id.str){doc._id=id.s
 db.boards.find().forEach(function(doc){ var id=doc._id; if(id.str){doc._id=id.str; db.boards.remove({_id:id}); db.boards.insert(doc);} });
 db.screens.update({type:'local'},{$set:{type:'message'}},{multi:true});
 db.users.find().forEach(function(doc){db.users.remove(doc);});
+db.screens.find({type:'message'}).forEach(function(doc){db.screens.update({_id:doc._id},{$set:{data:{message:doc.message}}})});
+db.screens.find().forEach(function(doc){db.screens.update({_id:doc._id},{$unset:{message:''}})});
