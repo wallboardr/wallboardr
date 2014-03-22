d3bd57128986519db797d7b61b97e071fe1634e8
Colin Bate <colin@colinbate.com>
2014-03-22 03:07:22 +0000

    Add config copying to install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 echo Installing Wallboardr
 npm install
 npm install ws
+cp config.template.json config.json
+cp plugins.template.json plugins.json
 hg clone https://bitbucket.org/colinbate/wallboardr-teamcity ./public/assets/plugins/teamcity
 hg clone https://bitbucket.org/colinbate/wallboardr-octopusdeploy ./public/assets/plugins/octopus
 touch node_modules/grunt-concurrent/index.js
